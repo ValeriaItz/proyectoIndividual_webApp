0629d62cf9867d188792e9c06996fe6810068704
unknown <UV@DGTI-COA830.coatza.intra.uv.mx>
2022-11-11 12:48:13 -0600

    visualizar preguntas

--- js/app.js
+++ js/app.js
@@ -1,63 +1,104 @@
-const quizBox = document.getElementById('quiz');
-const resultsBox = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-const myQuestions = [];
-
-for(i=0; i < data.length; i++) {
-    myQuestions.push(data[i]); // pushes the next question into our array. The loop cycles until it reaches the end of the dataset. In our case called ‘data’.
-}
-
 function buildQuiz(){
     const output = [];
-
+    
     myQuestions.forEach(
         (currentQuestion, questionNumber) => {
-        const answers = [];
+            const answers = [];
 
-        for(letter in currentQuestion.answers){
-            answers.push(
-            `<label>
-                <input type="radio" name="question${questionNumber}"
+            for(letter in currentQuestion.answers){
+                answers.push(
+                `<label> <input type="radio" name="question${questionNumber}"
                 value="${letter}" class="rad_butn">
                 ${letter} :
                 ${currentQuestion.answers[letter]}
-            </label>`
-            );
+                </label>`
+                );
+            }
 
             output.push(
                 `<div class="slide">
-                    <div class="question"> ${(questionNumber+1)}.
-                    ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join("")} </div>
+                <div class="question"> ${(questionNumber+1)}.
+                ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join("")} </div>
                 </div>`
             );
         }
-            
-        }
     );
-
     quizBox.innerHTML = output.join('');
 }
+
 
 function showResults(){
     const answerBoxs = quizBox.querySelectorAll('.answers');
     let numCorrect = 0;
     myQuestions.forEach( (currentQuestion, questionNumber) => {
-    const answerBox = answerBoxs[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerBox.querySelector(selector) || {}).value;
-    if(userAnswer === currentQuestion.correctAnswer){
-        numCorrect++;
-        answerBoxs[questionNumber].style.color = 'green';
-    }
-    else{
-        answerBoxs[questionNumber].style.color = 'red';
-    }
+        const answerBox = answerBoxs[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerBox.querySelector(selector) || {}).value;
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+            answerBoxs[questionNumber].style.color = 'green';
+        }
+        else{
+            answerBoxs[questionNumber].style.color = 'red';
+        }
     });
     resultsBox.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
 
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+        previousButton.style.display = 'none';
+    }
+    else{
+        previousButton.style.display = 'inline-block';
+    }
+    
+    if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+
+const quizBox = document.getElementById('quiz');
+const resultsBox = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const tryAgainButton = document.getElementById("tryAgain");
+
+const myQuestions = [];
+
+for(i=0; i < data.length; i++){
+    myQuestions.push(data[i]); // pushes the next question into our array. The loop cycles until it reaches the end of the dataset. In our case called ‘data’.
+}
+
+document.getElementById('quizLength').innerHTML = data.length;
+
 buildQuiz();
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+showSlide(currentSlide);
+
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
