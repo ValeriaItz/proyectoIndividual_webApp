e79523b0e55daa331016bb00ff3ebb8095cc74d4
Valeria Itzel <valeria.itz22@gmail.com>
2022-11-25 00:47:34 -0600

    editar preguntas agregado

--- js/addquestions.js
+++ js/addquestions.js
@@ -17,3 +17,43 @@ function getMyData(){
     }
     return questionBank;
 }
+
+
+/* =========================== FORM1  ======================= */
+function saveMyFile(){
+    localStorage.setItem("questionBank", JSON.stringify(questionBank));
+    location.replace("quiz.html")
+}
+
+const addQuestionButton = document.getElementById("submitQuestion");
+//reference the submitQuestion button and store it in a variable called addQuestionButton
+
+const saveButton = document.getElementById("saveQuestionBank");
+//reference the saveQuestionBank button and store it in a variable called
+bankButton
+
+const questionBank = []; //create a new empty array called questionBank
+
+const newObject = localStorage.getItem("questionBank"); //retrieve the question bank from localStorage
+
+let dataStored = JSON.parse(newObject); //parse the JSON data into a new JavaScript object
+
+if(dataStored != ""){ //if dataStored is not empty…
+    for(i=0; i < dataStored.length; i++){
+        questionBank.push(dataStored[i]);
+    }
+    //iterate through the dataStored array and push each question into the questionBank array
+} else {
+    for(i=0; i < data.length; i++){
+        questionBank.push(data[i]);
+    }
+}
+
+document.getElementById('qCount').innerHTML = questionBank.length;
+//update the <h1> title tag with the current number of questions in our question bank
+
+addQuestionButton.addEventListener("click", getMyData);
+//Listen for the submit form button and when clicked call or invoke the getMyData() function
+
+saveButton.addEventListener("click", saveMyFile);
+//Listen for the save question bank button and when clicked call (invoke) the saveMyFile() function
--- js/app.js
+++ js/app.js
@@ -82,6 +82,7 @@ const resultsBox = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 const tryAgainButton = document.getElementById("tryAgain");
 
+/* ----------------- CAMBIAR POR
 const myQuestions = [];
 
 for(i=0; i < data.length; i++){
@@ -89,6 +90,24 @@ for(i=0; i < data.length; i++){
 }
 
 document.getElementById('quizLength').innerHTML = data.length;
+*/
+
+const myQuestions = [];
+const newObject = localStorage.getItem("questionBank");
+let dataStored = JSON.parse(newObject);
+
+if(dataStored != ""){
+    for(i=0; i < dataStored.length; i++){
+        myQuestions.push(dataStored[i]);
+    }
+    document.getElementById('quizLength').innerHTML = dataStored.length;
+} else {
+    for(i=0; i < data.length; i++){
+        myQuestions.push(data[i]);
+    }
+    document.getElementById('quizLength').innerHTML = data.length;
+}
+
 
 buildQuiz();
 
@@ -103,45 +122,3 @@ submitButton.addEventListener('click', showResults);
 previousButton.addEventListener("click", showPreviousSlide);
 nextButton.addEventListener("click", showNextSlide);
 
-
-/* =========================== FORM  ======================= */
-function saveMyFile(){
-    localStorage.setItem("questionBank", JSON.stringify(questionBank));
-    location.replace("quiz.html")
-}
-
-const addQuestionButton = document.getElementById("submitQuestion");
-//reference the submitQuestion button and store it in a variable called addQuestionButton
-
-const saveButton = document.getElementById("saveQuestionBank");
-//reference the saveQuestionBank button and store it in a variable called
-bankButton
-
-const questionBank = [];
-//create a new empty array called questionBank
-
-const newObject = localStorage.getItem("questionBank");
-//retrieve the question bank from localStorage
-
-let dataStored = JSON.parse(newObject);
-//parse the JSON data into a new JavaScript object
-
-if(dataStored != ""){ //if dataStored is not empty…
-    for(i=0; i < dataStored.length; i++){
-        questionBank.push(dataStored[i]);
-    }
-    //iterate through the dataStored array and push each question into the questionBank array
-} else {
-    for(i=0; i < data.length; i++){
-        questionBank.push(data[i]);
-    }
-}
-
-document.getElementById('qCount').innerHTML = questionBank.length;
-//update the <h1> title tag with the current number of questions in our question bank
-
-addQuestionButton.addEventListener("click", getMyData);
-//Listen for the submit form button and when clicked call or invoke the getMyData() function
-
-saveButton.addEventListener("click", saveMyFile);
-//Listen for the save question bank button and when clicked call (invoke) the saveMyFile() function
